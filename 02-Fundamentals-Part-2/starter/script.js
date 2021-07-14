7e86658053efc52bc593b19fe580ca0ba20f96b1
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-13 21:58:42 -0700

    complete lesson 49(The While Loop)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -475,3 +475,23 @@
 //     console.log(`Exercise ${exercise}: Rep ${rep}`);
 //   }
 // }
+
+//! ~~~~~~~~~~~~~~~~~ 49) The While Loop ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`FOR: Lifting weights repition ${rep}`);
+// };
+
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`WHILE: Lifting weights repitition ${rep}`);
+//   rep++
+// };
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log(`Loop is about to end...`);
+// }
