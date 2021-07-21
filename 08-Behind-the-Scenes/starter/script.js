17187b8148183d61706912b35b8a1c4e1c1ed0b9
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-20 18:46:34 -0700

    add notes for Primitives Vs Objects

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -203,3 +203,21 @@
 //   return a + b;
 // };
 //? An arrow function DOES NOT have access to the arguments keyword
+
+//! ~~~~~~ PRIMITIVES VS OBJECTS (Primitives vs. Reference Types) ~~~~~~~~~~~~~~
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Nick',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend: ', friend);
+console.log('Me: ', me);
