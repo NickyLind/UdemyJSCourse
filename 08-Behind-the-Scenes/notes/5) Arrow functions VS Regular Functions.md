6a1de162bbc2ef21a7c1685f57b7a31acb64742e
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-20 17:59:54 -0700

    practice the differences between regular and arrow function and using the this keyword with each

---
+++ 08-Behind-the-Scenes/notes/5) Arrow functions VS Regular Functions.md
@@ -0,0 +1,1 @@
+# **_Regular Functions VS. Arrow Functions_**
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -132,3 +132,74 @@
 //? we can save the function in a variable like so
 // f();
 //? the 'this' keyword is now undefined because there is no object calling it
+
+//! ~~~~~Regular Functions VS. Arrow Functions~~~~~~~~~
+
+// var firstName = 'Matilda';
+
+// const nick = {
+//   firstName: 'Nick',
+//   year: 1990,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2020 - this.year);
+//   },
+// greet: () => console.log(`Hey ${this.firstName}`),
+//? arrow function doesn't get a 'this' keyword so it uses the parent's 'this' keyword (which is the window Object). Window.greet isn't a method so it returns 'undefined'
+// };
+// nick.greet();
+
+//!NOTE Why you should never use an Arrow Function as a method:
+//!NOTE if we add 'var firstName = 'Matilda';' up above, remember that 'var' creates a variable INSIDE the window Object, so our arrow function will therefore have a firstName property and the greet method will return the firstName 'Matilda' for the 'Nick' Object because the arrow function will return the 'this' of the parent Object (window). THIS IS BAD
+
+// var firstName = 'Matilda';
+//* PRE ES6 SOLUTION TO CALLING FUNCTIONS IN METHODS
+// const nick = {
+//   firstName: 'Nick',
+//   year: 1990,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2020 - this.year);
+//     const self = this;
+//     const isMillenial = function () {
+//console.log(self); //typically called 'self' or 'that'
+// console.log(this.year >= 1981 && this.year <= 1996);
+//? this returns undefined because the 'this' keyword in a regular function returns undefined, therefore we must create a 'self' variable and set it equal to 'this' outside of the scope of the function so we can then use 'self' in place of 'this' to reference the object
+//       console.log(self.year >= 1981 && self.year <= 1996);
+//     };
+//     isMillenial();
+//   },
+// };
+// nick.calcAge();
+
+//* POST ES6 VERSION OF CALLING FUNCTIONS IN METHODS (ARROW FUNCTION)
+
+// const nick = {
+//   firstName: 'Nick',
+//   year: 1990,
+//   calcAge: function () {
+//     const isMillenial = () => {
+//? simply changing the function from a declarative function to an arrow function works because arrow functions don't have 'this' so they would 'inherit' the 'this' from it's parent's scope
+//       console.log(this);
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//     };
+//     isMillenial();
+//   },
+// };
+// nick.calcAge();
+
+//* INFO ABOUT THE ARGUMENTS KEYWORD FOR FUNCTIONS
+
+// const addExpression = function (a, b) {
+//   console.log(arguments);
+//   return a + b;
+// };
+// addExpression(2, 5);
+// addExpression(2, 5, 8, 12);
+//? These extra arguments can be accessed via the arguments keyword in a function expression (above) or Declarative Function
+
+// var arrowExpression = (a, b) => {
+//   console.log(arguments);
+//   return a + b;
+// };
+//? An arrow function DOES NOT have access to the arguments keyword
