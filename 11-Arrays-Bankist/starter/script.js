d97c3c5a0791753940fe621d3fc1c07fb57a28d8
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-28 15:59:42 -0700

    Array Methods Practice

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -474,32 +474,32 @@ GOOD LUCK 😀
 
 //! ~~~~~~~ More Ways of Creating and Filling Arrays ~~~~~~~~
 
-//* ~~~ Empty arrays & Fill Method ~~~~~~
-//?NOTE creates an array with 7 empty elements
-const x = new Array(7);
-console.log(x);
+// //* ~~~ Empty arrays & Fill Method ~~~~~~
+// //?NOTE creates an array with 7 empty elements
+// const x = new Array(7);
+// console.log(x);
 
-//?NOTE the fill method fills an array with the inputted value (destructive)
-x.fill(1, 3, 5)
-//?NOTE the first parameter is the value being filled, the second and third parameters are the startin g and end points to fill the array
-console.log(x);
+// //?NOTE the fill method fills an array with the inputted value (destructive)
+// x.fill(1, 3, 5)
+// //?NOTE the first parameter is the value being filled, the second and third parameters are the startin g and end points to fill the array
+// console.log(x);
 
-//* ~~~~~ Array.from ~~~~~~~~~~~
+// //* ~~~~~ Array.from ~~~~~~~~~~~
 
-//?NOTE this method is called on the Array contructor itself
-const y = Array.from({length: 7}, () => 1);
-//?NOTE the first parameter is an object with a length property that defines the length of the array, the second parameter is a callback function that fills that array
-console.log(y);
+// //?NOTE this method is called on the Array contructor itself
+// const y = Array.from({length: 7}, () => 1);
+// //?NOTE the first parameter is an object with a length property that defines the length of the array, the second parameter is a callback function that fills that array
+// console.log(y);
 
-const z = Array.from({length: 7}, (_, i) => {
-  return i + 1
-});
+// const z = Array.from({length: 7}, (_, i) => {
+//   return i + 1
+// });
 
-console.log(z);
+// console.log(z);
 
-const movementsUI = Array.from(document.querySelectorAll('.movements__value'), el => el.textContent.replace('€', '$'))
+// const movementsUI = Array.from(document.querySelectorAll('.movements__value'), el => el.textContent.replace('€', '$'))
 
-console.log(movementsUI);
+// console.log(movementsUI);
 
 //! ~~~~~~~ SUMMARY: Which Array Method to Use? ~~~~~~~~~~
 
@@ -569,3 +569,90 @@ console.log(movementsUI);
 //*BASED ON CALLBACK:
 //* .forEach
 //* (does not create a new array just loops over it)
+
+
+//! ~~~~~~~~~~ ARRAY METHODS PRACTICE ~~~~~~~~~~~~~~
+
+//* 1) sum deposits of all accounts in bank
+// const bankDepositSum = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .filter(value => value > 0)
+//   .reduce((sum , cur) => sum + cur, 0)
+
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(value => value > 0)
+  .reduce((sum , cur) => sum + cur, 0)
+console.log(bankDepositSum);
+
+//* 2) How many deposits in the bank that are >= 1000
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >=  1000)
+//   .length
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0)
+console.log(numDeposits1000);
+
+let a = 10;
+//console.log(a++); //returns 10 because the increament after the variable doesn't return the incremented variable until it is called again
+console.log(++a);
+
+//* 3) Using reduce to boil down an array into an object (create an object which contains the sum of the deposits and withdrawals)
+
+const {deposits, withdrawals} = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((sums, cur) => {
+    // cur > 0 ? sums.deposits += cur : sums.withdrawals += cur; //*Refactor
+    sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur
+    return sums;
+  }, {deposits: 0, withdrawals: 0});
+
+console.log(deposits, withdrawals);
+
+//* 4) Create a function that will convert any string to a Title Case
+
+const convertTitleCase = function(title) {
+  const exceptions = ['a', 'an', 'the', 'is', 'but', 'or', 'in', 'with']
+
+  const titleCase = title
+    .toLowerCase()
+    .split(" ")
+    .map(word => exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1))
+    .join(" ")
+    return titleCase
+} 
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title, but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
+
+//! ~~~~~~~~~~~~~ CODING CHALLENGE #4 ~~~~~~~~~~~~~~~~~
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+]
+*/
