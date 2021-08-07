03459870feb4a5a0e42b851144a396f4dbd4fccf
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-07 13:03:12 -0700

    what is OOP?

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//! ~~~~~~~~~ What is Object-Orientated Programming ~~~~~~~~
+
+//?NOTE ABSTRACTION: ignoring or hiding details that don't matter, allowing us to get an overview perspective of the thing we're implementing, instead of messing with details that don't really matter to our implementation
+
+//?NOTE ENCAPSULATION: Keeping properties and methods private inside the class, so they are not accessible from outside the class. Some methods can be exposed as a public interface (API)
+
+//?NOTE INHERITANCE: Making all properties and methods of a certain class available to a child class, forming a hierarchical relationship between classes. This allows us to resuse common logic and to model real-world relationships
+
+//?NOTE POLYMORPHISM: A child class can overwrite a method it inherited from a parent class [it's more complex than that, but enough for our purposes]
