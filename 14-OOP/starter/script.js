9f0045f3c16d187785f5c14265800d08297fb29c
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-08 15:36:31 -0700

    Inheritance Between 'Classes': Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -565,3 +565,33 @@
 // const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 // martha.introduce();
 // martha.calcAge();
+
+//! ~~~~~~ Inheritance Between 'Classes': Object.create ~~~~~~~
+
+const PersonProto = {
+  calcAge() {
+    console.log(2021 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear)
+  this.course = course;
+};
+
+StudentProto.introduce = function() {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto)
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
