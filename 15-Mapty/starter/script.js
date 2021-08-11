10a3a365234a9013e82fb3b2837d7811664f183f
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-11 11:58:50 -0700

    Displaying a map using Leaflet Library

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -17,6 +17,18 @@ if(navigator.geolocation) {
     const { latitude } = position.coords
     const { longitude } = position.coords
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+    //?NOTE 'L' is the global variable object imported from the Leaflet script that gives us access to all it's methods.
+    //?NOTE the first parameter in the setView method is the coordinates, while the second parameter is the starting zoom on the coordinates
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
   }, function() {
     alert('Could not retrieve position')
   });
