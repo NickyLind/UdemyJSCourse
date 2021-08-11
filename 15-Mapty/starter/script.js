9339de34b8c93b424dbf8d80fea6a97eed5e7d2d
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-11 16:14:28 -0700

    Refactoring for Project Architecture

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,64 +11,85 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
 
-if(navigator.geolocation) {
-  //? the navigator.geolocation api's getCurrentPosition method takes 2 arguments. The first is a callback function for for when the call succeeds the 2nd is a callback function for when it fails
-  navigator.geolocation.getCurrentPosition(function(position) {
-    const { latitude } = position.coords
-    const { longitude } = position.coords
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  constructor() {
+    this._getPosition(); //We throw this in the constructor so that it runs as soon as an instance of the App Class is created
 
-    const coords = [latitude, longitude];
+    form.addEventListener('submit', this._newWorkout.bind(this)); // 'this' in an event listener always points to the DOM element it is attached to so we need to bind 'this' to our App object
+    inputType.addEventListener('change', this._toggleElevationField)
+  }
 
-    map = L.map('map').setView(coords, 13);
-    // console.log(map);
-    //?NOTE 'L' is the global variable object imported from the Leaflet script that gives us access to all it's methods.
-    //?NOTE the first parameter in the setView method is the coordinates, while the second parameter is the starting zoom on the coordinates
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    // Handling clicks on map
-    map.on('click', function(mapE) {
-      mapEvent = mapE; //sets the global mapEvent state to the event from this click handler
-      //?NOTE just like in JavaScript how we get access to an 'event' form an event listener, the 'on()' listener gives us access to an event using leaflet as well
-      form.classList.remove('hidden'); //remove our hidden from the form div to show it
-      inputDistance.focus(); //puts the cursor in the inputDistance query selection
+  _getPosition() {
+    if(navigator.geolocation) {
+      //? the navigator.geolocation api's getCurrentPosition method takes 2 arguments. The first is a callback function for for when the call succeeds the 2nd is a callback function for when it fails
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function() {
+        //?NOTE we need to bind the this keyword to the object that the '_loadMap' function belongs to
+        alert('Could not retrieve position')
+      });
+    };
+  }
 
-    });
-    //?NOTE 'map' is the variable we stored the loaded map data, here we call the 'on()' method that we recieve from the leaflet library
-  }, function() {
-    alert('Could not retrieve position')
-  });
-};
+  _loadMap(position) {
+      const { latitude } = position.coords
+      const { longitude } = position.coords
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      
+      console.log(this);
+      this.#map = L.map('map').setView(coords, 13);
+      // console.log(map);
+      //?NOTE 'L' is the global variable object imported from the Leaflet script that gives us access to all it's methods.
+      //?NOTE the first parameter in the setView method is the coordinates, while the second parameter is the starting zoom on the coordinates
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.#map);
+      // Handling clicks on map
+      this.#map.on('click', this._showForm.bind(this));
+      //?NOTE 'map' is the variable we stored the loaded map data, here we call the 'on()' method that we recieve from the leaflet library
+  }
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault() //need to prevent the default action of a form submit (page reload)
+  _showForm(mapE) {
+    this.#mapEvent = mapE; //sets the global mapEvent state to the event from this click handler
+        //?NOTE just like in JavaScript how we get access to an 'event' form an event listener, the 'on()' listener gives us access to an event using leaflet as well
+    form.classList.remove('hidden'); //remove our hidden from the form div to show it
+    inputDistance.focus(); //puts the cursor in the inputDistance query selection
+  }
 
-  // Clear Input Fields
-  inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = '';
+  _toggleElevationField() {
+      inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+      //? going toward parent elements, finds the closes parent with the 'form_row' class name and then toggles the 'form__row--hidden' class name off and on 
+      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-  // Display Marker
-  console.log(mapEvent);
-  const {lat, lng} = mapEvent.latlng;
-  //?NOTE we move the L.marker functionality into our submit event and destructure the lat and longitude from it 
-  L.marker([lat, lng])
-  .addTo(map)
-  .bindPopup(L.popup({
-    maxWidth: 250,
-    minWidth: 100,
-    autoClose: false,
-    closeOnClick: false,
-    className: 'running-popup'
-  }))
-  .setPopupContent('Placeholder text')
-  .openPopup();
-  form.classList.add('hidden');
-});
+  _newWorkout(e) {
+      e.preventDefault() //need to prevent the default action of a form submit (page reload)
 
-inputType.addEventListener('change', function() {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  //? going toward parent elements, finds the closes parent with the 'form_row' class name and then toggles the 'form__row--hidden' class name off and on 
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+      // Clear Input Fields
+      inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = '';
+    
+      // Display Marker
+      console.log(this.#mapEvent);
+      const {lat, lng} = this.#mapEvent.latlng;
+      //?NOTE we move the L.marker functionality into our submit event and destructure the lat and longitude from it 
+      L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        }))
+      .setPopupContent('Placeholder text')
+      .openPopup();
+  }
+}
+
+const app = new App(); // create an instance of our App Class
+
+
+
