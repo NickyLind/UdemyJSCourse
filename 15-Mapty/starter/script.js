8f9eefe947a403938885e922480bd23abff69d27
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-11 12:52:26 -0700

    Displaying a map marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -21,14 +21,30 @@ if(navigator.geolocation) {
     const coords = [latitude, longitude];
 
     const map = L.map('map').setView(coords, 13);
+    // console.log(map);
     //?NOTE 'L' is the global variable object imported from the Leaflet script that gives us access to all it's methods.
     //?NOTE the first parameter in the setView method is the coordinates, while the second parameter is the starting zoom on the coordinates
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    
+    map.on('click', function(mapEvent) {
+      const {lat, lng} = mapEvent.latlng
+      //?NOTE just like in JavaScript how we get access to an 'event' form an event listener, the 'on()' listener gives us access to an event using leaflet as well
+      L.marker([lat, lng]).addTo(map)
+      .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      }))
+      .setPopupContent('Placeholder text')
+      .openPopup();
+      //?NOTE we move the L.marker functionality into our click event and destructure the lat and longitude from it 
+      console.log(mapEvent);
+    });
+    //?NOTE 'map' is the variable we stored the loaded map data, here we call the 'on()' method that we recieve from the leaflet library
   }, function() {
     alert('Could not retrieve position')
   });
