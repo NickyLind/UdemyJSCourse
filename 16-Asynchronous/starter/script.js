9aef2ea656188df4212e39090a2e82ab477e2c6d
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-13 20:22:55 -0700

    Welcome to Callback Hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,32 +4,87 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountryData = function(country) {
+// const getCountryData = function(country) {
 
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`)
+//   request.send();
+  
+//   request.addEventListener('load', function() {
+//     const data = JSON.parse(this.responseText)[0]
+//     console.log(data);
+    
+//     const html = `
+//     <article class="country">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//     </article>
+//     `
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// }
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('Canada');
+
+//! ~~~~~~~~~~~~~~~~~~~~~ Welcome To Callback Hell ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const renderCountry = function(data, className) {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+  <h3 class="country__name">${data.name}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+  </article>
+  `
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighborData = function(country) {
+
+  // AJAX Call Country 1
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`)
   request.send();
   
   request.addEventListener('load', function() {
-    const data = JSON.parse(this.responseText)
-    console.log(data[0]);
-    
-    const html = `
-    <article class="country">
-    <img class="country__img" src="${data[0].flag}" />
-    <div class="country__data">
-    <h3 class="country__name">${data[0].name}</h3>
-    <h4 class="country__region">${data[0].region}</h4>
-    <p class="country__row"><span>👫</span>${(+data[0].population / 1000000).toFixed(1)} people</p>
-    <p class="country__row"><span>🗣️</span>${data[0].languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data[0].currencies[0].name}</p>
-    </div>
-    </article>
-    `
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    const [data] = JSON.parse(this.responseText) //?NOTE destructuring the array from the request
+    console.log(data);
+
+    // Render Country 1 
+    renderCountry(data);
+
+    // Get neighbor country (2)
+    const [neighbor] = data.borders;
+
+    if(!neighbor) return;
+
+    // AJAX Call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`)
+    request2.send();
+
+    request2.addEventListener('load', function() {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour')
+    });
   });
-}
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('Canada');
+};
+
+// getCountryAndNeighborData("portugal")
+getCountryAndNeighborData("usa")
