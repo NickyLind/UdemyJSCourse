d49061de470970975b872c42b3ab03bcadf75e3a
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-13 23:03:52 -0700

    Consuming Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -91,12 +91,55 @@ const countriesContainer = document.querySelector('.countries');
 
 //! ~~~~~~~~~~~~~~~~~ Promises and the Fetch API ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-//* Dated method
-  // const request = new XMLHttpRequest();
-  // request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`)
-  // request.send();
+// //* Dated method
+//   // const request = new XMLHttpRequest();
+//   // request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`)
+//   // request.send();
 
-//* Modern version using FetchAPI
+// //* Modern version using FetchAPI
 
-const request = fetch(`https://restcountries.eu/rest/v2/name/portugal`)
-console.log(request);
+// const request = fetch(`https://restcountries.eu/rest/v2/name/portugal`)
+// console.log(request);
+
+//! ~~~~~~~~~~~~~~~~~~~~~~ Consuming Promises ~~~~~~~~~~~~~~~~~~~~~~~
+const renderCountry = function(data, className) {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+  <h3 class="country__name">${data.name}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+  </article>
+  `
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+// const getCountryData = function(country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//   .then(function(response) {
+//     console.log(response); //?NOTE we recieve a response from the API
+//     return response.json(); //?NOTE the json() (asynchonrous, will also return a promise) method is available to all reponse objects coming from a fetch function
+//   }).then(function(data) {
+//     console.log(data); //?NOTE we grab the data from the response we called the json() method on (which was returned using a promise)
+//     data.forEach((element) => {
+//       renderCountry(element)
+//     })
+//     //* NOTE I called a foreach method on the data instead of simply using the 0th index to just return the first index (first country) of the data
+//   })
+// };
+
+
+//* CLEANED UP VERSION
+
+const getCountryData = function(country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => data.forEach((element) => renderCountry(element)))
+}
+
+getCountryData('portugal')
