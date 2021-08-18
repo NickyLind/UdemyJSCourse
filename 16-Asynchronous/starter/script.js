2c4a3aefbfcace7c1cb0ff139f3dd2011e57ce62
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-17 22:22:48 -0700

    Consuming Promises w/ Async/Await

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -610,3 +610,53 @@ const countriesContainer = document.querySelector('.countries');
 //     currentImg.style.display = 'none';
 //   })
 //   .catch(err => console.error(err));
+
+//! ~~~~~~~~~~~~~~~~~~~ Consuming Promises w/ Async/Await ~~~~~~~~~~~~~~~~~~~~~~~~
+const renderCountry = function(data, className) {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+  <h3 class="country__name">${data.name}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+  </article>
+  `
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function() {
+  return new Promise(function(resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = async function() {
+  // Geolocation
+  const pos = await getPosition();
+  const {latitude: lat, longitude: lng} = pos.coords
+
+  // Reverse Geocoding
+  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+  const dataGeo = await resGeo.json();
+  console.log(dataGeo);
+
+  // Country Data
+  const response = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
+  console.log(response);
+  
+  const data = await response.json();
+  console.log(data[0]);
+  renderCountry(data[0]);
+};
+
+  //?NOTE async/await is just syntatic sugar for consuming promises using .then()~~
+  // fetch(`https://restcountries.eu/rest/v2/name/${country}`).then( res => {return res.json()}).then(data => renderCountry(data[0]))
+  //?~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+whereAmI();
+console.log('FIRST'); 
