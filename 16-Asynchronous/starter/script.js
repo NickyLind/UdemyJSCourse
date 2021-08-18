d0f81b4467224bb2c62fd9ca20f0afbb23b01621
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-17 23:17:54 -0700

    Running Promises in Parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -739,80 +739,111 @@ const countriesContainer = document.querySelector('.countries');
 
 //! ~~~~~~~~~~~~~~~~~~~~~~~ Returning Values from Async Functions ~~~~~~~~~~~~~~~~~~
 
-const renderCountry = function(data, className) {
-  const html = `
-  <article class="country ${className}">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-  <h3 class="country__name">${data.name}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-  </article>
-  `
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
+// const renderCountry = function(data, className) {
+//   const html = `
+//   <article class="country ${className}">
+//   <img class="country__img" src="${data.flag}" />
+//   <div class="country__data">
+//   <h3 class="country__name">${data.name}</h3>
+//   <h4 class="country__region">${data.region}</h4>
+//   <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+//   <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//   <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+//   </article>
+//   `
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  countriesContainer.style.opacity = 1;
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const getPosition = function() {
-  return new Promise(function(resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
+// const getPosition = function() {
+//   return new Promise(function(resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
 
-const whereAmI = async function() {
-  try {
-    // Geolocation
-    const pos = await getPosition();
-    const {latitude: lat, longitude: lng} = pos.coords
+// const whereAmI = async function() {
+//   try {
+//     // Geolocation
+//     const pos = await getPosition();
+//     const {latitude: lat, longitude: lng} = pos.coords
 
-    // Reverse Geocoding
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    if(!resGeo.ok) throw new Error('"Problem getting location data"')
-    const dataGeo = await resGeo.json();
+//     // Reverse Geocoding
+//     const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     if(!resGeo.ok) throw new Error('"Problem getting location data"')
+//     const dataGeo = await resGeo.json();
 
-    // Country Data
-    const response = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
-    if(!response.ok) throw new Error('"Problem getting country data"')
+//     // Country Data
+//     const response = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
+//     if(!response.ok) throw new Error('"Problem getting country data"')
     
-    const data = await response.json();
-    renderCountry(data[0]);
+//     const data = await response.json();
+//     renderCountry(data[0]);
 
-    return `2: You are in ${dataGeo.city[0] + dataGeo.city.slice(1).toLowerCase()} ${dataGeo.state}, ${dataGeo.country}.`
+//     return `2: You are in ${dataGeo.city[0] + dataGeo.city.slice(1).toLowerCase()} ${dataGeo.state}, ${dataGeo.country}.`
+//   } catch(error) {
+//     console.error(`2: ERROR: ${error.message}`)
+//     renderError(`Something went wrong 😕 ${error.message}`)
+
+//     // Reject Promise returned from async function
+//     throw error;
+//     //?NOTE because we are returning the promise as fulfilled in line 788 as a result of this whereAmI function being called we have to throw the error manually since it never 
+//   }
+// };
+
+//   console.log('1: Will get location');
+// // const city = whereAmI();
+// // console.log(city);
+
+// //* Mixes using Async/await with Then/Catch
+// // whereAmI()
+// //   .then(city => console.log(city))
+// //   .catch(error => console.error(error))
+// //   .finally(() => console.log('3: Finished getting location'));
+
+//   //* IIFE (Immediately Invoked Function Expressions) (one of the last use-cases for one is this example)
+//   (async function() {
+//     try {
+//       const city = await whereAmI();
+//       console.log(city);
+//     } catch(error) {
+//       console.error(`2: ERROR: ${error.message}`)
+//     }
+//     console.log('3: Finished getting lcoation');
+//   })();
+
+//! ~~~~~~~~~~~~~~~~~~~~~ Running Promises in Parallel ~~~~~~~~~~~~~~~~~~~~~~~
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+}
+
+const get3Countries = async function(c1, c2, c3) {
+  try{
+    // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`)
+    // const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`)
+    // const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
+    ])
+    //?NOTE this function takes in an array of promises, and it will return a new promise, which will then run all the promises in the array at the same time
+    // console.log([data1.capital, data2.capital, data3.capital]);
+    console.log(data);
+    console.log(data.map(data => data[0].capital));
+    //! If one promise rejects, then the entire Promise.all() function will reject
   } catch(error) {
-    console.error(`2: ERROR: ${error.message}`)
-    renderError(`Something went wrong 😕 ${error.message}`)
-
-    // Reject Promise returned from async function
-    throw error;
-    //?NOTE because we are returning the promise as fulfilled in line 788 as a result of this whereAmI function being called we have to throw the error manually since it never 
+    console.error(`ERROR: ${error.message}`)
   }
-};
-
-  console.log('1: Will get location');
-// const city = whereAmI();
-// console.log(city);
-
-//* Mixes using Async/await with Then/Catch
-// whereAmI()
-//   .then(city => console.log(city))
-//   .catch(error => console.error(error))
-//   .finally(() => console.log('3: Finished getting location'));
-
-  //* IIFE (Immediately Invoked Function Expressions) (one of the last use-cases for one is this example)
-  (async function() {
-    try {
-      const city = await whereAmI();
-      console.log(city);
-    } catch(error) {
-      console.error(`2: ERROR: ${error.message}`)
-    }
-    console.log('3: Finished getting lcoation');
-  })();
+}
+get3Countries('portugal', 'canada', 'tanzania')
