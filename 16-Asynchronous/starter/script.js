b2f20bf2c46439d84c5cb3cf02b5f865d91d3bf9
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-17 23:40:05 -0700

    Other Promise Combinators: race, allSettled, and any

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -818,32 +818,104 @@ const countriesContainer = document.querySelector('.countries');
 //   })();
 
 //! ~~~~~~~~~~~~~~~~~~~~~ Running Promises in Parallel ~~~~~~~~~~~~~~~~~~~~~~~
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+//     return response.json();
+//   });
+// }
+
+// const get3Countries = async function(c1, c2, c3) {
+//   try{
+//     // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`)
+//     // const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`)
+//     // const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
+
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+//       getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+//       getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
+//     ])
+//     //?NOTE this function takes in an array of promises, and it will return a new promise, which will then run all the promises in the array at the same time
+//     // console.log([data1.capital, data2.capital, data3.capital]);
+//     console.log(data);
+//     console.log(data.map(data => data[0].capital));
+//     //! If one promise rejects, then the entire Promise.all() function will reject
+//   } catch(error) {
+//     console.error(`ERROR: ${error.message}`)
+//   }
+// }
+// get3Countries('portugal', 'canada', 'tanzania')
+
+//! ~~~~~~~~~ Other Promise Combinators: race, allSettled, and any ~~~~~~~~~~~~~~~
+//?NOTE all combinators recieve an array of promises and then return a single promise  
+
 const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then(response => {
     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
     return response.json();
   });
-}
+};
 
-const get3Countries = async function(c1, c2, c3) {
-  try{
-    // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`)
-    // const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`)
-    // const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
+//* Promise.race()
+//?NOTE the first settled promise gets returned (whether resovled or rejected)
+(async function() {
+  const response = await Promise.race([
+    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+    getJSON(`https://restcountries.eu/rest/v2/name/mexico`)
+  ]);
+  console.log(response[0]);
+})();
 
-    const data = await Promise.all([
-      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
-      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
-      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
-    ])
-    //?NOTE this function takes in an array of promises, and it will return a new promise, which will then run all the promises in the array at the same time
-    // console.log([data1.capital, data2.capital, data3.capital]);
-    console.log(data);
-    console.log(data.map(data => data[0].capital));
-    //! If one promise rejects, then the entire Promise.all() function will reject
-  } catch(error) {
-    console.error(`ERROR: ${error.message}`)
-  }
-}
-get3Countries('portugal', 'canada', 'tanzania')
+
+//* Example of setting a timeout in a race method so a slow internet connection will timeout rather than wait forever for the fetch
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const timeout = function(sec) {
+  return new Promise(function(_, reject) {
+    setTimeout(function() {
+      reject(new Error('request took too long'))
+    }, sec * 1)
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+  timeout(1)
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err))
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//* Promise.allSettled()
+//?NOTE takes in an array of promises and will return an array of all SETTLED promises (whether rejected or not)
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Failed'),
+  Promise.resolve('Another Success')
+])
+  .then(res => console.log(res))
+
+
+//* Promise.all
+//?NOTE takes in an array of promises and will return either all resolved promises or will short circuit and show an error
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('Failed'),
+  Promise.resolve('Another Success')
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err))
+
+//* Promise.any [ES2021]
+//?NOTE takes in an array of multiple Promsies and will return the first fulfilled promise, ignoring rejected promises
+
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('Failed'),
+  Promise.resolve('Another Success')
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err))
