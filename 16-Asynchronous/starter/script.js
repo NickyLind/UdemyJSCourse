bb026422f478a5e08dd418153714b102bd63c0ab
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-18 00:01:20 -0700

    CODING CHALLENGE #3

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -849,73 +849,183 @@ const countriesContainer = document.querySelector('.countries');
 // get3Countries('portugal', 'canada', 'tanzania')
 
 //! ~~~~~~~~~ Other Promise Combinators: race, allSettled, and any ~~~~~~~~~~~~~~~
-//?NOTE all combinators recieve an array of promises and then return a single promise  
+// //?NOTE all combinators recieve an array of promises and then return a single promise  
 
-const getJSON = function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-    return response.json();
+//     return response.json();
+//   });
+// };
+
+// //* Promise.race()
+// //?NOTE the first settled promise gets returned (whether resovled or rejected)
+// (async function() {
+//   const response = await Promise.race([
+//     getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+//     getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+//     getJSON(`https://restcountries.eu/rest/v2/name/mexico`)
+//   ]);
+//   console.log(response[0]);
+// })();
+
+
+// //* Example of setting a timeout in a race method so a slow internet connection will timeout rather than wait forever for the fetch
+// //* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// const timeout = function(sec) {
+//   return new Promise(function(_, reject) {
+//     setTimeout(function() {
+//       reject(new Error('request took too long'))
+//     }, sec * 1)
+//   });
+// };
+
+// Promise.race([
+//   getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+//   timeout(1)
+// ])
+//   .then(res => console.log(res[0]))
+//   .catch(err => console.error(err))
+// //* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// //* Promise.allSettled()
+// //?NOTE takes in an array of promises and will return an array of all SETTLED promises (whether rejected or not)
+// Promise.allSettled([
+//   Promise.resolve('Success'),
+//   Promise.reject('Failed'),
+//   Promise.resolve('Another Success')
+// ])
+//   .then(res => console.log(res))
+
+
+// //* Promise.all
+// //?NOTE takes in an array of promises and will return either all resolved promises or will short circuit and show an error
+// Promise.all([
+//   Promise.resolve('Success'),
+//   Promise.reject('Failed'),
+//   Promise.resolve('Another Success')
+// ])
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err))
+
+// //* Promise.any [ES2021]
+// //?NOTE takes in an array of multiple Promsies and will return the first fulfilled promise, ignoring rejected promises
+
+// Promise.any([
+//   Promise.resolve('Success'),
+//   Promise.reject('Failed'),
+//   Promise.resolve('Another Success')
+// ])
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err))
+
+//! ~~~~~~~~~~~~~~~~~~~~~~~ CODING CHALLENGE #3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+const createImage = function(imgPath) {
+  return new Promise(function(resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function() {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function() {
+      reject(new Error('Image not found'))
+    });
   });
 };
 
-//* Promise.race()
-//?NOTE the first settled promise gets returned (whether resovled or rejected)
-(async function() {
-  const response = await Promise.race([
-    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
-    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
-    getJSON(`https://restcountries.eu/rest/v2/name/mexico`)
-  ]);
-  console.log(response[0]);
-})();
-
-
-//* Example of setting a timeout in a race method so a slow internet connection will timeout rather than wait forever for the fetch
-//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const timeout = function(sec) {
-  return new Promise(function(_, reject) {
-    setTimeout(function() {
-      reject(new Error('request took too long'))
-    }, sec * 1)
+const wait = function(seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
   });
 };
 
-Promise.race([
-  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
-  timeout(1)
-])
-  .then(res => console.log(res[0]))
-  .catch(err => console.error(err))
-//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//* Promise.allSettled()
-//?NOTE takes in an array of promises and will return an array of all SETTLED promises (whether rejected or not)
-Promise.allSettled([
-  Promise.resolve('Success'),
-  Promise.reject('Failed'),
-  Promise.resolve('Another Success')
-])
-  .then(res => console.log(res))
+const imgContainer = document.querySelector('.images');
 
 
-//* Promise.all
-//?NOTE takes in an array of promises and will return either all resolved promises or will short circuit and show an error
-Promise.all([
-  Promise.resolve('Success'),
-  Promise.reject('Failed'),
-  Promise.resolve('Another Success')
-])
-  .then(res => console.log(res))
-  .catch(err => console.error(err))
+//* PART 1
+const loadNPause = async function () {
+  try {
+    // Load Image 1
+    let img = await createImage('img/img-1.jpg');
+    console.log('image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
 
-//* Promise.any [ES2021]
-//?NOTE takes in an array of multiple Promsies and will return the first fulfilled promise, ignoring rejected promises
+    // Load Image 2
+    img = await createImage('img/img-2.jpg');
+    console.log('image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
 
-Promise.any([
-  Promise.resolve('Success'),
-  Promise.reject('Failed'),
-  Promise.resolve('Another Success')
-])
-  .then(res => console.log(res))
-  .catch(err => console.error(err))
+    // Load Image 3
+    img = await createImage('img/img-3.jpg');
+    console.log('image 3 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+  } catch(err) {
+    console.error(`ERROR: ${err.message}`)
+  }
+}
+// loadNPause();
+
+//* PART 2
+const loadAll = async function(imgArr) {
+  try{
+    const imgs = imgArr.map(async img => await createImage(img));
+    // console.log(imgs);
+
+    const imgsEl = await Promise.all(imgs)
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add('parallel'))
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'])
+
+
+
+// let currentImg;
+
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('image loaded');
+//     return wait(2)
+// })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('image loaded');
+//     return wait(2)
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+//   .catch(err => console.error(err));
