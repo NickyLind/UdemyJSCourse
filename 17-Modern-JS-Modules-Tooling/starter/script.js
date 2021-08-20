7b9788b408baa392b42adedd6f5aed5ee961fcc2
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-19 17:10:13 -0700

    Configuring Babel and Polyfilling

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -29,3 +29,24 @@ if(module.hot) {
 }
 //?NOTE this allows us to only fire specific modules so we can save state (not having to relog user etc) (in our example anytime the page reloads this module persists we repeatedly are adding items to our cart)
 //?NOTE this code is code that only parcel understands so it won't make it into our final bundled code because the browser will not understand it
+
+class Person {
+  #greeting = 'Hey'
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person('Jonas')
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+//?NOTE Polyfilling
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+//?NOTE Polyfilling async functions
+import 'regenerator-runtime/runtime';
